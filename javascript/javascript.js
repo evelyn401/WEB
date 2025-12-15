ee791599937645bd1eb32e482b01f6e50083b039
evelyn401 <evelynlin401@gmail.com>
2025-12-15 13:03:16 +0800

    feat: 新增年份變數以改善程式碼可讀性

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,5 @@
 // 取得 html 元素(id)
 // 新增變數 year 縮短程式碼
-let year =document.getElementById("Year");
-year.textContent = new Date().getFullYear();
+let year = document.getElementById("Year");
+let years = new Date().getFullYear();
+year.textContent = years;
