73caa800c68765f42dddd0d31035d67cfa0d0339
evelyn401 <evelynlin401@gmail.com>
2025-12-24 12:24:34 +0800

    feat: 更新 Carousel.js 的註解，修正自動輪播及手動切換圖片的問題

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,4 +5,4 @@ import { BannerCarousel } from "./module/Carousel.js";
 // 執行函式
 // 參數為 html 元素 id
 Years("Year");
-BannerCarousel();
+// BannerCarousel();
--- javascript/module/Carousel.js
+++ javascript/module/Carousel.js
@@ -1,4 +1,15 @@
 // 廣告圖輪播
+
+/*
+    初始進入頁面，每兩秒自動輪播圖片，並且可以透過前後按鈕切換圖片
+
+    修正 
+    1.手動點擊時，會中斷自動輪播的問題
+    2.若三秒內沒有手動點擊，則繼續自動輪播
+    3.連續快速點擊按鈕時：
+    不會導致圖片閃爍的問題，setTimeout()應確保 setInterval() 不會與手動切換圖片的函式衝突，只能有一個在運行
+ */
+
 export function BannerCarousel() {
     // 宣告變數 設定為圖片的索引
     let currentIndex = 0;
@@ -6,6 +17,12 @@ export function BannerCarousel() {
     const Images = document.querySelectorAll( "#Carousel > .CarouselImg" );
     // 宣告常數 讀取圖片的長度（張數）
     const totalImages = Images.length;
+    
+    // 20251224新增資料
+    // 存放自動播放的計時器
+    let autoPlayInterval;
+    // 存放延遲重啟的計時器
+    let restartTimeout;
 
     // 顯示下一張圖片的函式
     function showNextImage() {
