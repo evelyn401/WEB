05ccc8819094be7c96c89bd5d845952d104af22e
evelyn401 <evelynlin401@gmail.com>
2025-12-26 14:48:08 +0800

    feat: 在javascript.js中新增NavBar函數的執行

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,10 @@
 // 匯入函式
 import { Years } from "./module/AllDate.js";
 import { BannerCarousel } from "./module/Carousel.js";
+import { NavBar } from "./module/navbar.js";
 
 // 執行函式
 // 參數為 html 元素 id
 Years("Year");
 BannerCarousel();
+NavBar();
