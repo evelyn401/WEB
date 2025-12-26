bb426547ff2203f0088fbc5418a9aa02c6af74fa
evelyn401 <evelynlin401@gmail.com>
2025-12-26 14:21:18 +0800

    feat: 在NavBar函數中新增MenuLinks陣列以支持導航連結

--- javascript/module/navbar.js
+++ javascript/module/navbar.js
@@ -1,4 +1,5 @@
 export function NavBar(){
     // 宣告陣列，將元素存入陣列之中;陣列資料，讀取「索引值」，索引值從0開始
     let MenuItems = ["About", "Skill", "Works", "Contact"]; 
+    let MenuLinks = ["#Abouts", "#Skills", "#Works", "#Contacts"];
 }
